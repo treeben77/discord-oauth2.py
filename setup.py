a57c86da656c4caed3afd9e4dd098ec77e2357e0
TreeBen77 <77905642+TreeBen77@users.noreply.github.com>
2021-07-04 15:52:21 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='0.2a',
+      description='DiscordOAuth2.py',
+      author='TreeBen77',
+      author_email='treeben77yt@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['requets'],
+      classifiers=[
+          'Development Status :: 2 - Alpha',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Python Software Foundation License',
+          'Programming Language :: Python',
+          'Topic :: Communications :: Email',
+          ],
+     )
