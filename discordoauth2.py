e26d5b9a15672ed3b3bb0665bfd1a5085862b3cd
TreeBen77 <77905642+TreeBen77@users.noreply.github.com>
2022-01-28 15:37:02 +1000

    Added guilds_member and refresh token support
    You can now use the guilds.member scope! I've also added support to collect refresh tokens and a method to get a new access token from the refresh token.

--- discordoauth2.py
+++ discordoauth2.py
@@ -6,36 +6,42 @@ class token_instance():
     self.bot_token = bot_token
   
   def identify(self):
-    try:
-      headers = {
-       'Authorization': f'Bearer {self.token}'
-      }
-      user_object = requests.get(url="https://discordapp.com/api/users/@me",headers=headers)
-      return user_object.json()
-    except(requests.exceptions.HTTPError) as error:
-      return error
+    headers = {
+      'Authorization': f'Bearer {self.token}'
+    }
+    user_object = requests.get(url="https://discordapp.com/api/v9/users/@me",headers=headers)
+    if user_object.status_code == 401: raise Exception(f"Scope wasn't granted in OAuth2.")
+    if user_object.status_code == 429: raise Exception(f"You are being Rate Limited. Retry after: {user_object.json()['retry_after']}")
+    return user_object.json()
 
   def connections(self):
-    try:
-      headers = {
-        'Authorization': f'Bearer {self.token}'
-      }
-      user_object = requests.get(url="https://discordapp.com/api/users/@me/connections",headers=headers)
-      return user_object.json()
-    except(requests.exceptions.HTTPError) as error:
-      return error
+    headers = {
+      'Authorization': f'Bearer {self.token}'
+    }
+    user_object = requests.get(url="https://discordapp.com/api/v9/users/@me/connections",headers=headers)
+    if user_object.status_code == 401: raise Exception(f"Scope wasn't granted in OAuth2.")
+    if user_object.status_code == 429: raise Exception(f"You are being Rate Limited. Retry after: {user_object.json()['retry_after']}")
+    return user_object.json()
 
   def guilds(self):
-    try:
-      headers = {
-          'Authorization': f'Bearer {self.token}'
-        }
-      user_object = requests.get(url="https://discordapp.com/api/users/@me/guilds",headers=headers)
-      return user_object.json()
-    except(requests.exceptions.HTTPError) as error:
-      return error
+    headers = {
+        'Authorization': f'Bearer {self.token}'
+      }
+    user_object = requests.get(url="https://discordapp.com/api/v9/users/@me/guilds",headers=headers)
+    if user_object.status_code == 401: raise Exception(f"Scope wasn't granted in OAuth2.")
+    if user_object.status_code == 429: raise Exception(f"You are being Rate Limited. Retry after: {user_object.json()['retry_after']}")
+    return user_object.json()
 
-  def join_guild(self, guild):
+  def guilds_member(self, guild):
+    headers = {
+        'Authorization': f'Bearer {self.token}'
+      }
+    user_object = requests.get(url=f"https://discordapp.com/api/v9/users/@me/guilds/{guild}/member",headers=headers)
+    if user_object.status_code == 401: raise Exception(f"Scope wasn't granted in OAuth2.")
+    if user_object.status_code == 429: raise Exception(f"You are being Rate Limited. Retry after: {user_object.json()['retry_after']}")
+    return user_object.json()
+  
+  def guilds_join(self, guild):
     try:
       headers = {
         'Authorization': f'Bot {self.bot_token}',
@@ -44,13 +50,21 @@ class token_instance():
       data = {
         'access_token': self.token
       }
-      user_object = requests.put(url=f"https://discordapp.com/api/guilds/{guild}/members/{self.identify}", json=data, headers=headers)
-      return user_object.text
+      user_object = requests.put(url=f"https://discordapp.com/api/v9/guilds/{guild}/members/{self.identify()['id']}", json=data, headers=headers)
+      if user_object.status_code == 401: raise Exception(f"Scope wasn't granted in OAuth2.")
+      if user_object.status_code == 429: raise Exception(f"You are being Rate Limited. Retry after: {user_object.json()['retry_after']}")
+      if user_object.status_code == 403: raise Exception(f"Provided token cannot invite people on Guild: {guild}. Guild ID may be incorrect.")
+      return user_object
     except(requests.exceptions.HTTPError) as error:
       return error
 
+class access_token():
+  def __init__(self, response, token):
+    self.access = token_instance(response["access_token"], token)
+    self.expires = response["expires_in"]
+    self.refresh_token = response["refresh_token"]
+
 class discordOauth2():
-  endpoint = 'https://discord.com/api/v8'
   def __init__(self, client, secret, redirect, token=None):
     self.client = client
     self.secret = secret
@@ -58,15 +72,22 @@ class discordOauth2():
     self.token = token
   
   def exchange_code(self, token):
-    data = {
+    response = requests.post("https://discord.com/api/v9/oauth2/token", data={
       'client_id': self.client,
       'client_secret': self.secret,
       'grant_type': 'authorization_code',
       'code': token,
       'redirect_uri': self.redirect
-    }
-    headers = {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    data = requests.post(discordOauth2.endpoint+'/oauth2/token', data=data, headers=headers).json()
-    return token_instance(data['access_token'], self.token)
+    })
+    if response.status_code == 429: raise Exception(f"You are being Rate Limited")
+    return access_token(response.json(), self.token)
+  
+  def refresh_token(self, refresh_token):
+    response = requests.post("https://discord.com/api/v9/oauth2/token", data={
+      'client_id': self.client,
+      'client_secret': self.secret,
+      'grant_type': 'refresh_token',
+      'refresh_token': refresh_token
+    })
+    if response.status_code == 429: raise Exception(f"You are being Rate Limited")
+    return access_token(response.json(), self.token)
