94dfcd9dba71c82bca49332f376147b1ee4713bd
TreeBen77 <77905642+TreeBen77@users.noreply.github.com>
2022-12-04 01:50:49 +1000

    prepared things for pypi

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setup(
+    name='discordoauth2',
+    description='Use Discord\'s OAuth2 effortlessly! Turns the auth code to a access token and the access token into scope infomation. ',
+    version="1.0.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    python_requires='>=3.8',
+    author="TreeBen77",
+    packages=[
+        'discordoauth2'
+    ],
+    url='https://github.com/TreeBen77/discordoauth2',
+    keywords='flask, oauth2, discord, discord-api',
+    install_requires=[
+        'requests'
+    ]
+)
