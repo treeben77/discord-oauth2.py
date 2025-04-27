986cf870855360da310bb445048e79cda51f2a5d
treeben77 <77905642+treeben77@users.noreply.github.com>
2025-04-27 18:54:51 +1000

    bump patch version to 1.2.2

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as file:
 setup(
     name="discord-oauth2.py",
     description="Use Discord's OAuth2 effortlessly! Turns the auth code to a access token and the access token into scope infomation. ",
-    version="1.2.1",
+    version="1.2.2",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
     python_requires=">=3.8",
     author="TreeBen77",
     packages=["discordoauth2"],
-    url="https://github.com/TreeBen77/discordoauth2",
+    url="https://github.com/treeben77/discordoauth2",
     keywords="flask, oauth2, discord, discord-api",
     install_requires=[
         "aiohttp",
